9702767b68c14658a59238b8ed76b49e8fc472e9
niksankarkee <karkeeniksan@gmail.com>
2021-06-08 13:24:54 +0900

    component n databinding stater

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,19 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { WarningAlertComponent } from './warningAlert/warningAlert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
-import { AssignmentComponent } from './assignment/assignment.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent,
-    WarningAlertComponent,
-    SuccessAlertComponent,
-    AssignmentComponent,
+    AppComponent
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }

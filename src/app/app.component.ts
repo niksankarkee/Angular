95ff806e5602a94cc872fa3a67eca4acd2cfa324
niksankarkee <karkeeniksan@gmail.com>
2021-06-08 13:05:34 +0900

    practise project component setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'from-documentation';
+  title = 'practical-project-one';
 }
